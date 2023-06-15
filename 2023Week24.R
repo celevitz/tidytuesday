45fc16172281ceee2b05150a0e7a08d07d570299
Carly <celevitz@gmail.com>
2023-06-14 17:38:52 -0700

    add labels to some of the bars

--- 2023Week24.R
+++ 2023Week24.R
@@ -46,58 +46,59 @@ table(rawdata$affect_conflicts)
     mutate(N=n()) %>%
     select(yearslived,affectedbyconflicts,n,N) %>%
     distinct() %>%
-    mutate(percent = n/N)
+    mutate(percent = n/N
+           ,graphlabel = paste0(round(percent*100,1),"%\n(n=",n,")"))
 
 ## Set up the graph stuff
   xaxistitle <- "Percent of households"
   yaxistitle <- "Years lived in this village"
-  captiontext <- "Note: 'Years lived' was categorized by quartiles"
-  maintitle <- "How is length of time lived in a village related to frequency of conflicts?"
+  maintitle <- "How is length of time lived in a village related to conflict frequency?"
+  subtitletext <- "Interviews took place between Nov. 2016 and June 2017 in the villages of Chirodzo, God, and Ruaca"
 
-  captionsource <- str_glue("Visualization: Twitter @carlylevitz &bull; Source: github.com/celevitz/tidytuesday &bull; Tools: #rstats #ggplot #tidyverse")
-  facetcaption <- captiontext <- str_glue("{captiontext}<br>{captionsource}")
+  captiontext <- str_glue("Tidy Tuesday 2023 Week 23 /// Note: 'Years lived' was categorized by quartiles.
+                          Source: Data comes from the SAFI (Studying African Farmer-Led Irrigation) survey, a subset of the data used in the Data Carpentry Social Sciences workshop.
+                          Citation: Woodhouse, P; Veldwisch, GJ; Brockington, D; Komakech, HC; Manjichi, A; Venot, JP (2018): SAFI Survey Results. doi:10.6084/m9.figshare.6262019.v1
+                          Visualization: Twitter @carlylevitz /// code: github.com/celevitz/tidytuesday /// Tools: #rstats #ggplot #tidyverse #ggtext")
 
 
 ## Graph it
-  data %>% ggplot(aes(x=percent,y=yearslived,fill=affectedbyconflicts,label=n)) +
+  data %>% ggplot(aes(x=percent,y=yearslived,fill=affectedbyconflicts)) +
     geom_bar(stat="identity",color="gray20") +
+    annotate("text",x=0.1,y=data$yearslived[data$affectedbyconflicts == "never"]
+             ,label=data$graphlabel[data$affectedbyconflicts == "never"]
+             ,color="gray20",hjust=0,size=5) +
+    annotate("text",x=.49
+             ,y=data$yearslived[data$affectedbyconflicts == "more than once"]
+             ,label=data$graphlabel[data$affectedbyconflicts == "more than once"]
+             ,color="gray20",hjust=0,size=5) +
+    annotate("text",x=.94
+             ,y=data$yearslived[data$affectedbyconflicts == "unknown"]
+             ,label=data$graphlabel[data$affectedbyconflicts == "unknown"]
+             ,color="gray20",hjust=1,size=5) +
     scale_fill_manual(values=c("#C5D6D8","#60695C","#ABDF75","#99F7AB","#D6F9DD")) +
     xlab(xaxistitle) + ylab(yaxistitle) +
     scale_x_continuous(lim=c(0,1),breaks=seq(0,1,.2),labels=paste0(round(seq(0,1,.2)*100,1),"%"))+
-    scale_y_discrete(labels=c(paste0(fourthquartile," to ",max," years")
-                              ,paste0(median," to ",fourthquartile," years")
-                              ,paste0(secondquartile," to ",median," years")
-                              ,paste0(firstquartile," to ",secondquartile," years") )) +
-    labs(title = maintitle,caption = captiontext) +
-    guides(fill=guide_legend(title="Frequency") ) +
+    scale_y_discrete(labels=c(paste0("[",fourthquartile," to ",max,"] years")
+                              ,paste0("[",median," to ",fourthquartile,") years")
+                              ,paste0("[",secondquartile," to ",median,") years")
+                              ,paste0("[",firstquartile," to ",secondquartile,") years") )) +
+    labs(title = maintitle,subtitle = subtitletext,caption = captiontext) +
+    guides(fill=guide_legend(title="Frequency of conflicts") ) +
     theme_minimal() +
     theme(panel.grid = element_blank()
           ,panel.background = element_rect(color="beige", fill="beige")
           ,plot.background  = element_rect(color="beige", fill="beige")
-          ,plot.title       = element_markdown(color = "gray20",size = 32, face = "bold")
+          ,plot.title       = element_markdown(color = "gray20",size = 32, face = "bold",margin = margin(t=10,b=10,l=-140))
           ,plot.margin       = margin(t=10,r=40,b=10,l=10)
           ,axis.line.x      = element_line(color = "gray20")
           ,axis.text        = element_markdown(color="gray20",size=20)
           ,axis.title        = element_markdown(color="gray20",size=20,face = "bold")
-          ,plot.caption     = element_text(size=20,hjust=1,color="gray20")
+          ,plot.caption     = element_text(size=12,hjust=.5,color="gray20")
+          ,plot.subtitle     = element_text(size=20,hjust=.5,color="gray20")
           ,legend.key.size = unit(2, 'cm')
           ,legend.title = element_markdown(size=15,color="gray20",face = "bold")
           ,legend.text = element_text(size=15,color="gray20") )
 
 dev.print(png, file = paste(savedirectory,"TidyTuesday2023Week24.png",sep=""), width = 1100, height = 900)
 dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
